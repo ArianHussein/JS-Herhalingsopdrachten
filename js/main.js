0d872f2869d5db23f42f5bbb1a9246075fc66046
ArianHussein <302453552@student.rocmondriaan.nl>
2023-11-10 10:22:33 +0100

    Herhalingsopdrachten JS

---
+++ js/main.js
@@ -0,0 +1,64 @@
+//Opdracht 1
+
+
+
+
+
+
+
+
+//Opdracht 2
+
+
+
+
+
+
+
+//Opdracht 3
+
+
+
+
+
+
+
+//Opdracht 4
+
+
+
+
+
+
+//Opdracht 5
+
+
+
+
+
+
+
+//Opdracht 6
+
+
+
+
+
+
+
+//Opdracht 7
+
+
+
+
+
+
+//Opdracht 8
+
+
+
+
+
+//Opdracht 9
+const colours = ["rood", "groen", "geel", "blauw", "roze"];
+console.log(colours);
