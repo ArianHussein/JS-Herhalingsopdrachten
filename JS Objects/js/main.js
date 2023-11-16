470c274375c3cd06912eec56e8eb91272f5312aa
ArianHussein <302453552@student.rocmondriaan.nl>
2023-11-16 21:39:15 +0100

    Opdrachten Objects

---
+++ JS Objects/js/main.js
@@ -0,0 +1,146 @@
+//Opdracht 1
+
+const player1 = {
+    fullname: "Peter",
+    lastname: "Van Donk",
+    age: 19,
+    opleiding: "Software Developer",
+    score: 45,
+
+};
+console.log(player1);
+
+//Opdracht 2
+
+
+const facebook = {
+    username: "Steve221",
+    title: "man bijt hond",
+    verhaal: "Man ging boodschappen doen en toen zag hij een hond en hij begon hem inneens te bijten",
+    datum: "03-02-2069",
+    views: 6969,
+};
+console.log(facebook);
+
+
+//Opdracht 3
+
+const ingredients = {
+    title: "How to make pancake in 5 steps",
+    description: "Gooi pannenkoek in pan en de rest komt vanzelf",
+    healthy: "het is gezond",
+};
+console.log(ingredients);
+const broodjemetbeleg = {
+    title: "Beleg op broodje",
+    description: "blabla dit is tekst",
+    healthy: "beleg is gezond",
+};
+console.log(broodjemetbeleg);
+const cornflakes = {
+    title: "Cocopops cornflakes",
+    description: "Gooi cornflakes in bakje met melk",
+    healthy: "cornflakes very nice",
+};
+console.log(cornflakes);
+let recept = [ingredients, broodjemetbeleg, cornflakes]
+console.log(recept);
+
+//Opdracht 4
+
+const toyota1 = {
+    title: "Toyota supra mk4",
+    description: "Een zeer zeldzaam Toyota Supra MK4 in goede staat.",
+    price: 37.350,
+    km: 170.812,
+    buildyear: 1994,
+};
+console.log(toyota1);
+
+const nissan2 = {
+    title: "Nissan GT-R",
+    description: "Prachtige nissan uit 2016..",
+    price: 119.900,
+    km: 53.240,
+    buildyear: 2016,
+};
+console.log(nissan2);
+
+const mazda3 = {
+    title: "Mazda RX7",
+    description: "Goed rijdende mazda 4x7 met de originele wankel motor loopt zeer mooi nette auto.",
+    price: 11.250,
+    km: 108.300,
+    buildyear: 1989,
+};
+console.log(mazda3);
+
+const honda4 = {
+    title: "Honda NSX",
+    description: "Een merk wat je meteen terugbrengt in memory lane.",
+    price: 112.500,
+    km: 46.139,
+    buildyear: 1992,
+};
+console.log(honda4);
+
+const audi5 = {
+    title: "Audi RS7 sportbacl",
+    description: "Speciaal voor audio sport modellen.",
+    price: 219.700,
+    km: 1.112,
+    buildyear: 2023,
+};
+console.log(audi5);
+
+//Opdracht 5
+
+const food1 = {
+    title: "Kakifruit",
+    description: "1 kg",
+    actie: "per fruit 1,89",
+    price: 1.89,
+
+};
+console.log(food1);
+
+const food2 = {
+    title: "Mandarijnen",
+    description: "1.5 kg",
+    actie: "per zak mandarijnen 2,49",
+    price: 2.49,
+};
+console.log(food2);
+
+const food3 = {
+    title: "Snoeptomaten",
+    description: "per emmer",
+    actie: "per emmer snoeptomaten 2,49",
+    price: 2.49,
+};
+console.log(food3);
+
+const food4 = {
+    title: "Bio + Uien",
+    description: "500 g",
+    actie: "per bio + uien 59",
+    price: 59,
+};
+console.log(food4);
+
+const food5 = {
+    title: "Hollandse Winterpeen",
+    description: "1 kg",
+    actie: "4 stuks 7,90",
+    price: 7.90,
+};
+console.log(food5);
+
+//Opdracht 6
+//Opdracht 7
+//Opdracht 8
+
+let foods = [food1, food2, food3, food4, food5]
+console.log(foods);
+//Opdracht 9
+
