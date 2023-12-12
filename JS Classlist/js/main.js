f8b93d9006cca98203c80c5900ea8da20d3ba1ea
ArianHussein <302453552@student.rocmondriaan.nl>
2023-12-12 14:55:19 +0100

    Opdrachten gemaakt

---
+++ JS Classlist/js/main.js
@@ -0,0 +1,57 @@
+// const box = document.querySelector(".box");
+// const addBtn = document.querySelector(".add-btn")
+// const removeBtn = document.querySelector(".remove-btn")
+
+// addBtn.addEventListener("click", function(){
+//     box.classList.add("bg-aqua");
+//     box.classList.add("border-red");
+// });
+
+// removeBtn.addEventListener("click", function (){
+//     box.classList.remove("bg-aqua");
+    
+// });
+
+// setInterval(function(){
+//     box.classList.toggle("bg-aqua");
+
+// }, 5000);
+
+//Opdr 1
+
+//  const box = document.querySelector(".box");
+//   const runBtn = document.querySelector(".run")
+
+//   runBtn.addEventListener("click", function(){
+//     box.classList.add("error")
+//   });
+
+  //Opdr 2
+//   const changeBtn = document.querySelector(".change")
+//   changeBtn.addEventListener("click", function(){
+//     box.classList.remove("error")
+//   });
+
+  //Opdr 3
+//  const box = document.querySelector(".box");
+//   const toggleBtn = document.querySelector(".toggle");
+
+
+
+//  toggleBtn.addEventListener("click", function(){
+//     box.classList.toggle("error");
+//   });
+z
+
+  //Opdr 5
+
+const animateBox = document.querySelector(".animate-box");
+const animateBtn = document.querySelector(".animate")
+
+animateBtn.addEventListener("click", function(){
+    animateBox.classList.toggle("blinker");
+})
+
+  
+  
+
